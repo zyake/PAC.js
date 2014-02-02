17f27b976bcd139f6fa0858c26a9f45b21a6cc09
zyake <zyake.mk4@gmail.com>
2014-02-02 14:08:40 +0900

    コメントを修正、ビルドスクリプトを強化

--- src/js/view/Application.js
+++ src/js/view/Application.js
@@ -1,4 +1,13 @@
 
+/**
+ *  A abstraction of a whole application
+ *
+ * It can accommodate all widgets that consist of a SPA(Single Page  Application).
+ * By starting a "Application" instance, Each widget can be accessed by a hash URL.
+ * - for example
+ * http://apphost/webapp#widget1 -> widget1
+ * http://apphost/webapp#widget2 -> widget2
+ */
 Application = {
 
    create: function(appElem, widgetDef) {
@@ -17,8 +26,8 @@ Application = {
     start: function(initWidgetId) {
         var me = this;
         window.addEventListener("hashchange", function(event) {
-            var hashLocation = event.newURL.lastIndexOf("#");
-            var newWidgetId = event.newURL.substring(hashLocation + 1);
+            var hashIndex = event.newURL.lastIndexOf("#");
+            var newWidgetId = event.newURL.substring(hashIndex + 1);
              me.transitionManager.transit(newWidgetId);
         });
         this.transitionManager.transit(initWidgetId);
