273a0d49783bdcfb24a4d1bc854393cef4bdb08b
zyake <zyake.mk4@gmail.com>
2014-04-09 20:54:05 +0900

    - Fixed README

--- src/js/view/1_Application.js
+++ src/js/view/1_Application.js
@@ -3,10 +3,39 @@
  *  An abstraction of a whole application
  *
  * It can accommodate all widgets that consist of a SPA(Single Page  Application).
- * By starting an "Application" instance, Each widget can be accessed by a hash URL.
+ *
+ * You can register widget definitions as follows.
+ * Application.create({
+ *  // Specify an application id.
+ *  id: "app1",
+ *
+ *  // Specify a container element.
+ *  appElem:  document.querySelector("#appContainer"),
+ *
+ *  // Specify widget definitions.
+ *  widgetDef: {
+ *      search: SearchWidget,
+ *      settings: SettingsWidget
+ *  }
+ * });
+ *
+ * An "Application" instance uses single "container" element to maintain widgets.
+ * So, all of widgets that were loaded will be stored in the "container" element.
+ * - for example
+ * <!-- An application container element  -->
+ * <div id="appContainer">
+ *     <!-- The loaded "search" widget-->
+ *     <div id="search">～</div>
+ *     <!-- The loaded "setting" widget -->
+ *     <div id="setting">～</div>
+ * </div>
+ *
+ * By starting an "Application" instance, each widget can be accessed by a hash URL.
  * - for example
  * http://apphost/webapp#widget1 -> widget1
  * http://apphost/webapp#widget2 -> widget2
+ *
+ * If a hash url has been already specified, it is used for navigation.
  */
 Application = {
 
