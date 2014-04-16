6f2a2574e7145aa214c50553e01158bdff18da6b
zyake <zyake.mk4@gmail.com>
2014-04-16 22:34:48 +0900

    - Add header.js - Move test resources

---
+++ src/js/header.js
@@ -0,0 +1,10 @@
+/**
+ * PAC.js version 0.0.1
+ *
+ * Copyright (C) 2014 zyake All Rights Reserved.
+ *
+ * For more information, please visit the following github page.
+ * https://github.com/zyake/PAC.js
+ */
+
+"use strict";
